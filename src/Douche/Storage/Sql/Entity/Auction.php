ad9e132efaf37c66384da9aae3c27647018493c6
Dave Marshall <dave.marshall@atstsolutions.co.uk>
2013-05-10 20:44:37 +0100

    Remove incorrect comment

--- src/Douche/Storage/Sql/Entity/Auction.php
+++ src/Douche/Storage/Sql/Entity/Auction.php
@@ -8,10 +8,6 @@ use Douche\Entity\Auction as BaseAuction;
 
 class Auction extends BaseAuction
 {
-    /**
-     * This could really be promoted to the public api, I'm sure we can find a 
-     * use for it
-     */
     public function addBid(User $bidder, Bid $bid)
     {
         $this->bids[] = [$bidder, $bid];
