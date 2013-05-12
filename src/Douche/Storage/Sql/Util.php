c4fedd41d47b896693c895c58578c412aede1b32
Dave Marshall <dave.marshall@atstsolutions.co.uk>
2013-05-13 00:33:44 +0100

    Extract schema generation in to Util class

---
+++ src/Douche/Storage/Sql/Util.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Douche\Storage\Sql;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Schema\Schema;
+
+class Util 
+{
+    public static function createAuctionSchema(Connection $conn)
+    {
+        $sm = $conn->getSchemaManager();
+
+        $schema = static::getAuctionSchema();
+        foreach ($schema->getTables() as $table) {
+            $sm->createTable($table);
+        }
+    }
+
+    public static function getAuctionSchema()
+    {
+        $schema = new Schema;
+        $auctions = $schema->createTable('auctions');
+        $auctions->addColumn('id', 'integer');
+        $auctions->setPrimaryKey(array("id"));
+        $auctions->addColumn('name', 'string');
+        $auctions->addColumn('ending_at', 'datetime');
+        $auctions->addColumn('currency', 'string');
+
+        $auctionBids = $schema->createTable('auction_bids');
+        $auctionBids->addColumn('id', 'integer');
+        $auctionBids->setPrimaryKey(array("id"));
+        $auctionBids->addColumn('auction_id', 'integer');
+        $auctionBids->addColumn('user_id', 'string');
+        $auctionBids->addColumn('amount', 'integer');
+        $auctionBids->addColumn('currency', 'string');
+        $auctionBids->addColumn('original_amount', 'integer');
+        $auctionBids->addColumn('original_currency', 'string');
+
+        return $schema;
+    }
+    
+}
--- tests/integration/Douche/Storage/Sql/SqlTestCase.php
+++ tests/integration/Douche/Storage/Sql/SqlTestCase.php
@@ -3,6 +3,7 @@
 namespace tests\integration\Douche\Storage\Sql;
 
 use Doctrine\DBAL\Schema\Table;
+use Douche\Storage\Sql\Util;
 
 class SqlTestCase extends \PHPUnit_Framework_TestCase 
 {
@@ -16,26 +17,7 @@ class SqlTestCase extends \PHPUnit_Framework_TestCase
         ];
 
         $this->conn = \Doctrine\DBAL\DriverManager::getConnection($params);
-        
-        $sm = $this->conn->getSchemaManager();
 
-        $auctions = new \Doctrine\DBAL\Schema\Table("auctions");
-        $auctions->addColumn('id', 'integer');
-        $auctions->setPrimaryKey(array("id"));
-        $auctions->addColumn('name', 'string');
-        $auctions->addColumn('ending_at', 'datetime');
-        $auctions->addColumn('currency', 'string');
-        $sm->createTable($auctions);
-
-        $auctionBids = new \Doctrine\DBAL\Schema\Table("auction_bids");
-        $auctionBids->addColumn('id', 'integer');
-        $auctionBids->setPrimaryKey(array("id"));
-        $auctionBids->addColumn('auction_id', 'integer');
-        $auctionBids->addColumn('user_id', 'string');
-        $auctionBids->addColumn('amount', 'integer');
-        $auctionBids->addColumn('currency', 'string');
-        $auctionBids->addColumn('original_amount', 'integer');
-        $auctionBids->addColumn('original_currency', 'string');
-        $sm->createTable($auctionBids);
+        Util::createAuctionSchema($this->conn);
     }
 }
