207873e706f6593ee6885766fba41ad89c1887c9
Igor Wiedler <igor@wiedler.ch>
2013-05-16 01:04:45 +0200

    CLI and templates

--- src/DoucheWeb/app.php
+++ src/DoucheWeb/app.php
@@ -2,18 +2,53 @@
 
 namespace DoucheWeb;
 
+use Douche\Interactor\AuctionListResponse;
+use Douche\Interactor\AuctionViewRequest;
+use Douche\Interactor\AuctionViewResponse;
+
+use Mustache\Silex\Provider\MustacheServiceProvider;
+
 use Silex\Application;
 use Silex\Provider\DoctrineServiceProvider;
 use Silex\Provider\MonologServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelEvents;
+
 $app = new Application();
 
 $app->register(new MonologServiceProvider());
 $app->register(new DoctrineServiceProvider());
+$app->register(new MustacheServiceProvider());
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new ServiceProvider());
 
-$app->get('/', 'interactor.auction_list:__invoke');
+$app->get('/', 'interactor.auction_list:__invoke')
+    ->value('controller', 'auction_list');
+
+$app->get('/auction/{id}', 'interactor.auction_view:__invoke')
+    ->value('controller', 'auction_view')
+    ->convert('request', function ($_, Request $request) {
+        return new AuctionViewRequest($request->attributes->get('id'));
+    });
+
+// TODO change to ->on once fabpot/silex#705 is merged
+$app['dispatcher'] = $app->share($app->extend('dispatcher', function ($dispatcher, $app) {
+    $dispatcher->addListener(KernelEvents::VIEW, function ($event) use ($app) {
+        $view = $event->getControllerResult();
+
+        $request = $event->getRequest();
+        $controller = $request->attributes->get('controller');
+        $template = "$controller.html";
+
+        $body = $app['mustache']->render($template, $view);
+        $response = new Response($body);
+        $event->setResponse($response);
+    });
+
+    return $dispatcher;
+}));
 
 return $app;
